aacf71ef9ac4bbcc1a1555ea7043b835a8c56e6c
Farid BESSAI <farid.bessai@aptushealth.com>
2020-10-31 18:25:40 +0100

    Improve exploration rate and refactor code

--- dqn.py
+++ dqn.py
@@ -14,27 +14,58 @@ from keras.utils import np_utils
 from collections import deque
 
 
+class State:
 
-
+    def __init__(self, obs):
+        self.obs= obs
+        self.next_state = None
+        self.action = None
+        self.reward = 0.0
+        self.done = False
+        
+        
+    def q_value(self, nn, discount, n_steps):
+        if( self.next_state == None  or self.done):
+            return self.reward
+        
+        
+        future_reward = 0.0
+        if n_steps<=1 :
+            q_values = nn.predict(np.array([self.next_state.obs]), verbose=0)
+            future_reward = np.amax(q_values,axis=1)[0]
+        else:
+            future_reward = self.next_state.q_value(nn, discount,n_steps-1)
+        
+        return self.reward + discount * future_reward
+    
+    def next_state(self):
+        return self.next_state
+    
+    def set_next_state(self, state):
+        self.next_state = state
+    
 class Dqn:
 
 
-    def __init__(self, action_space, state_space):
+    def __init__(self, action_space, state_size, layer_sizes, learn_eps_decay, learn_action_depth, learn_action_discount):
 
         self.action_space = action_space
-        self.state_space = state_space
+        self.state_size = state_size
+        self.layer_sizes = layer_sizes
+        
+        self.learn_action_depth =  learn_action_depth
         
         self.lr = 0.001  # learning rate 
-        self.discount = .99 # discount factor of the future rewards
+        self.discount = learn_action_discount # discount factor of the future rewards
         
         self.eps = 1.0 # initial exploration rate. Its value evolves during learning
-        self.eps_decay = .993  # the decrease rate of the exploration rate
+        self.eps_decay = learn_eps_decay  # the decrease rate of the exploration rate
         self.eps_min = .01 # the minimum exploration rate allowed 
         
         
-        self.memory = deque(maxlen=10000) # the states transition memory
-        self.batch_size = 100  # the number of transition used for updateing the model 
-        self.min_samples_to_learn= 1000 #  minimum number of transition in the memory before to start learning
+        self.memory = deque(maxlen=40000) # the states transition memory
+        self.batch_size = 3000  # the number of transition used for updateing the model 
+        self.min_samples_to_learn= 3000 #  minimum number of transition in the memory before to start learning
         
         self.nn = self.build_nnet()
         
@@ -42,63 +73,91 @@ class Dqn:
     def build_nnet(self):
 
         nn = Sequential()
-        nn.add(Dense(150, activation='relu', input_shape=( self.state_space,)))
-        nn.add(Dense(120, activation='relu'))
-        nn.add(Dense(self.action_space, activation='linear'))
+        nn.add(Dense(self.layer_sizes[0], activation='relu', input_shape=( self.state_size,)))
+        
+        for layer_size in self.layer_sizes:
+            nn.add(Dense(layer_size, activation='relu'))
+            
+        nn.add(Dense(self.action_space.n, activation='linear'))
         nn.compile(loss='mse', optimizer=optimizers.Adam(lr=self.lr))
         return nn
     
-    def addToMemory(self, state, action, reward, nextState, done):
-        self.memory.append((state, action, reward, nextState, done));
+    def addToMemory(self, state):
+        self.memory.append(state);
     
     def memorySize(self):
         return len(self.memory)
+    
+    #    avgReward:  the averagge reward so far. used to adjust the explorartion factor
+    def learn(self, avgReward):
         
-    def learn(self):
-        
-        batch_size =  self.memorySize()
-        if(batch_size<self.min_samples_to_learn):
+
+        if(self.memorySize()<self.min_samples_to_learn):
             return
-            
-        minibatch = random.sample(self.memory,batch_size)
-        states, actions, reward, nextStates, dones = zip(*minibatch)
-        states = np.asarray(states)
+        
+  
+        states = random.sample(self.memory,self.batch_size)
+        
+        
+        obs = [state.obs for state in states]
+        obs = np.asarray(obs)
+        
+        actions = [state.action for state in states]
         actions = np.asarray(actions)
+        
+        reward = [state.reward for state in states]
         reward = np.asarray(reward)
-        nextStates = np.asarray(nextStates)
+
+        dones = [state.done for state in states]
         dones = np.asarray(dones)
         
         targetReward = reward # by default, which corresponds to DONE
         
         #The value of the rewards should be updated only in case this is not the final step (DONE=False) 
-        predActionsRewards = self.nn.predict(nextStates, verbose=0)
-        pred2 = np.amax(predActionsRewards,axis=1)
-        pred3 = (reward + self.discount * pred2)[np.invert(dones)]
-        targetReward[np.invert(dones)] = pred3
+#        predActionsRewards = self.nn.predict(next_obs, verbose=0)
+#        
+#        pred2 = np.amax(predActionsRewards,axis=1)
+#        pred3 = (reward + self.discount * pred2)[np.invert(dones)]
+#        targetReward[np.invert(dones)] = pred3
         
-        output = self.nn.predict(states, verbose=0)
+        
+        targetReward = [state.q_value(self.nn, self.discount, self.learn_action_depth) for state in states]
+        
+        output = self.nn.predict(obs, verbose=0)
+
         output[np.arange(0,len(output)), actions] = targetReward
         
         
-        self.nn.fit(states, output, epochs=1, verbose=0)
+        
+        self.nn.fit(obs, output, epochs=1, verbose=0)
+        
+        
+
         if self.eps > self.eps_min:
-            self.eps *= self.eps_decay
+            eps = (self.eps * self.eps_decay)  if (avgReward > 0 or self.eps >.20) else (self.eps / self.eps_decay)
+            if eps <= 1.0:
+                self.eps = eps
 
         print('------- Epsilon: '+ str(self.eps))
-    def computeAction(self, state):
+        
+        
+    def computeAction(self, obs, explore):
     
         selected = 0
-        if np.random.rand() <= self.eps:
-            selected = random.randrange(self.action_space)
+        
+        if explore and np.random.rand() <= self.eps:
+            # exploration
+            selected = self.action_space.sample()
         else:
         
-            mstate = np.array([state]) #because predict takes on a collection of states
+            mstate = np.array([obs]) #because predict takes on a collection of states
             rawAction = self.nn.predict(mstate, verbose=0)
+
         
 
-            selected = np.argmax(rawAction[0])
+            selected = np.argmax(rawAction[0])  # get the index of the item having the max value
             
-        actions = np.zeros(self.action_space, dtype=bool)
+        actions = np.zeros(self.action_space.n, dtype=bool)
         actions[selected]=True
         
         
---
+++ lunar_lander_v2.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 22:19:48 2019
+
+@author: fbessai
+"""
+import numpy as np
+import gym
+from dqn import Dqn, State
+from matplotlib import pyplot as plt
+
+
+NB_EPISODES = 1000
+NB_STEPS_PER_EPISODE = 1000
+
+LEARN_NN_SIZE = [80, 150]
+LEARN_EPS_DECAY = .993 # .993
+LEARN_ACTION_DEPTH = 5
+LEARN_ACTION_DISCOUNT = .99  #.99
+
+
+
+RESULT_TOTALS = [] # set of the accumulated reward per episode
+
+
+env = gym.make('LunarLander-v2') # create gym the environment
+
+
+print(env.action_space.n)
+print(env.observation_space.shape[0])
+
+nnet = Dqn(env.action_space, env.observation_space.shape[0], LEARN_NN_SIZE, LEARN_EPS_DECAY, LEARN_ACTION_DEPTH, LEARN_ACTION_DISCOUNT)
+
+        
+        
+
+class Action:  # action space 
+    DO_NOTHING  = 0 
+    FIRE_LEFT   = 1
+    FIRE_BOTTOM = 2
+    FIRE_RIGHT  = 3
+    ACTIONS = np.array([DO_NOTHING,FIRE_LEFT, FIRE_BOTTOM,FIRE_RIGHT])
+    
+    def getActionFromVec(actionVec):
+        return  np.select( actionVec, Action.ACTIONS)
+    
+
+TARGET = State([0,0]);
+
+
+
+def dqn_policy(state, explore): # determines the action to take given the state
+   
+    actions = nnet.computeAction(state, explore)
+    
+
+    return Action.getActionFromVec(actions)
+
+
+
+for episode in range(NB_EPISODES):
+    
+    print('------- Episode : '+ str(episode))
+    episode_rewards = 0 
+    state = State(env.reset()) # initial state of the environement 
+    action = Action.DO_NOTHING 
+    
+    learn = episode <= 400;
+    
+
+    for step in range (NB_STEPS_PER_EPISODE): # max number of steps for one episode
+        
+        
+        action = dqn_policy(state.obs, learn) # compute the action to perform based on the current state of the enviroent 
+        
+        env.render() # show the scene
+            
+        next_state, reward, done, info = env.step(action) # perform the action and get the reward and new state
+        
+        state.action = action
+        state.reward = reward
+        state.done =  done
+        
+        state.next_state = State(next_state)
+        
+        if(learn):
+            nnet.addToMemory(state) # memorize the step transition values
+        
+        
+        state = state.next_state
+        
+        
+        episode_rewards += reward
+        
+        if done:
+
+            RESULT_TOTALS.append(episode_rewards)
+            break
+    
+    if(learn):    
+        nnet.learn(np.average(RESULT_TOTALS)) 
+
+    plt.plot(RESULT_TOTALS)
+    plt.show()
+   
+print(RESULT_TOTALS)
+print('Max reward : ' + str(max(RESULT_TOTALS)))
+
+plt.plot(RESULT_TOTALS)
+plt.show()
+
+env.close()
+env.reset()
